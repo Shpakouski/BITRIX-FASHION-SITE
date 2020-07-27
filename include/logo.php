662e598748b08f086bfe838b1f661950205a4778
Shpakouski <job.shpakouski@gmail.com>
2020-07-27 21:43:36 +0300

    added component for logo

---
+++ include/logo.php
@@ -0,0 +1,3 @@
+<div class="logo">
+ <a href="/"><img width="164" alt="logo.png" src="/upload/medialibrary/ada/ada5f1c5251807603f41910eb3fce6b6.png" height="51" title="logo.png" class="img-responsive"></a>
+</div>
